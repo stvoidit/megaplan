535d71a6573e10a787ade4928a028f8bb4d82ff4
StVoid <stvoid.it@yandex.ru>
2020-06-23 14:41:46 +0300

    добавлена щагрузка файла, возвращает список объектов, который могут быть добавлены в json как вложения

--- MegaplanAPI.go
+++ MegaplanAPI.go
@@ -1,9 +1,13 @@
 package megaplan
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 
@@ -17,7 +21,7 @@ func NewClien(domain string) *APImegaplan {
 			TokenURL: fmt.Sprintf("https://%s/api/v3/auth/access_token", domain),
 		},
 	}
-	return &APImegaplan{cnf: &cnf}
+	return &APImegaplan{cnf: &cnf, domain: domain}
 }
 
 func (mp *APImegaplan) setClient() {
@@ -26,8 +30,9 @@ func (mp *APImegaplan) setClient() {
 
 // APImegaplan - клиент для работы с мегаплан v3, обертка над oauth2
 type APImegaplan struct {
-	cnf *oauth2.Config
-	ts  oauth2.TokenSource
+	domain string
+	cnf    *oauth2.Config
+	ts     oauth2.TokenSource
 	*http.Client
 }
 
@@ -77,4 +82,70 @@ func saveToken(t *oauth2.Token, filename string) error {
 	}
 	defer w.Close()
 	return json.NewEncoder(w).Encode(t)
+}
+
+// UploadFiles - загрузка файла, в ответ приходит список из объектов,
+// которые могут быть переданы в json запросе как сущности File для содания вложения
+func (mp *APImegaplan) UploadFiles(files ...UploadFile) (attachments []Attachment, err error) {
+	if len(files) == 0 {
+		return nil, nil
+	}
+	var buf bytes.Buffer
+	mw := multipart.NewWriter(&buf)
+	for _, file := range files {
+		fw, err := mw.CreateFormFile("files[]", file.Filename)
+		if err != nil {
+			return nil, err
+		}
+		if _, err := io.Copy(fw, file.R); err != nil {
+			return nil, err
+		}
+	}
+	if err := mw.Close(); err != nil {
+		return nil, err
+	}
+	resp, err := mp.Post(fmt.Sprintf("https://%s/api/file", mp.domain), mw.FormDataContentType(), &buf)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var ats metaAttachments
+	if err := json.NewDecoder(resp.Body).Decode(&ats); err != nil {
+		return nil, err
+	}
+	return ats.Data, ats.Error()
+}
+
+// UploadFile - файл для загрузки
+type UploadFile struct {
+	Filename string
+	R        io.Reader
+}
+
+// Attachment - вложение
+type Attachment struct {
+	ContentType string `json:"contentType"`
+	ID          string `json:"id"`
+}
+
+type metaAttachments struct {
+	MetaInfo `json:"meta"`
+	Data     []Attachment `json:"data"`
+}
+
+// MetaInfo - meta из ответа API
+type MetaInfo struct {
+	Status uint64   `json:"status"`
+	Errors []string `json:"errors"`
+}
+
+func (mi *MetaInfo) Error() error {
+	if len(mi.Errors) == 0 {
+		return nil
+	}
+	var msgs string
+	for _, errMsg := range mi.Errors {
+		msgs += errMsg
+	}
+	return errors.New(msgs)
 }
